ecda569464b23b286517bcd8371deaee753b7c27
nehcneb <nehc.neb@gmail.com>
2024-04-26 21:01:31 +1000

    Update

--- pages/OWN.py
+++ pages/OWN.py
@@ -893,9 +893,12 @@ If this program produces an error (in red) or an unexpected spreadsheet, please 
             mime= "application/json", 
         )
 
-        st.write("If your spreadsheet reader does not display non-English text properly, please change the encoding to UTF-8 Unicode. For Excel for Mac, please click on:")
+        st.write("If your spreadsheet reader does not display non-English text properly, please change the encoding to UTF-8 Unicode. For Microsoft Excel, please see:")
 
         st.link_button("Excel for Mac encoding guide", "https://answers.microsoft.com/en-us/msoffice/forum/all/excel-for-mac-not-opening-utf-8-csv-files/67d951d3-b616-4f7d-9d9a-af76045f7947")
+
+        st.link_button("Excel for Windows encoding guide", "https://support.microsoft.com/en-au/office/text-import-wizard-c5b02af6-fda1-4440-899f-f78bafe41857#ID0EBBJ=Newer_versions")
+
 
 
 # %%
