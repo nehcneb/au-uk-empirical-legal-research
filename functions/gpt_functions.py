9603a4986a2e78d2df055889f1d944acbe8d5131
nehcneb <nehc.neb@gmail.com>
2025-04-15 09:23:07 +1000

    Update

--- functions/gpt_functions.py
+++ functions/gpt_functions.py
@@ -84,7 +84,7 @@ from functions.common_functions import check_questions_answers, pop_judgment, de
 
 # %%
 #Default models to use
-basic_model = 'gpt-4.1-nano'
+basic_model = 'gpt-4.1-mini'
 flagship_model = 'gpt-4.1'
 
 # %%
